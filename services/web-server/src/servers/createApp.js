ad6c5f4e4f986a0cf7f9e24af24fae633be32a45
Matt Boris <mboris@mozilla.com>
2022-01-25 09:45:01 -0500

    Specifically limit authorization endpoint

--- services/web-server/src/servers/createApp.js
+++ services/web-server/src/servers/createApp.js
@@ -5,7 +5,7 @@ const session = require('express-session');
 const compression = require('compression');
 const cors = require('cors');
 const express = require('express');
-const RateLimit = require('express-rate-limit');
+const rateLimit = require('express-rate-limit');
 const playground = require('graphql-playground-middleware-express').default;
 const passport = require('passport');
 const url = require('url');
@@ -135,17 +135,14 @@ module.exports = async ({ cfg, strategies, auth, monitor, db }) => {
     getCredentials,
   } = oauth2(cfg, db, strategies, auth, monitor);
 
-  // set up rate limiter: maximum # requests per minute
-  const limiter = new RateLimit({
+  // set up auth rate limiter: maximum # requests per minute
+  const authLimiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 1 minute
     max: cfg.app.rateLimitMaxRequests,
   });
 
-  // apply rate limiter to all requests
-  app.use(limiter);
-
   // 1. Render a dialog asking the user to grant access
-  app.get('/login/oauth/authorize', cors(corsOptions), authorization);
+  app.get('/login/oauth/authorize', authLimiter, cors(corsOptions), authorization);
   // 2. Process the dialog submission (skipped if redirectUri is whitelisted)
   app.post('/login/oauth/authorize/decision', cors(corsOptions), decision);
   // 3. Exchange code for an OAuth2 token
